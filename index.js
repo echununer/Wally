6ccdb5ce750b7f39f666647b35a10d87762f0424
echununer <echununerpro@gmail.com>
2025-09-10 10:39:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ let lista = generarEspacio(100);
 console.log (lista)
 
 for (let i = 0; i < lista.length; i++) {
+   if (lista[i].indexOf ("W") == 0) {
+      console.log (lista [i])
+   }
+}
+
+for (let i = 0; i < lista.length; i++) {
    if (lista[i] === "Wally") {
       console.log("Wally se encuentra en la posición " + i);
+   }
+}
+
+for (let i = 0; i < lista.length; i++) {
+   if (lista[i].includes("ll")){
+      console.log (lista [i])
    }
 }
